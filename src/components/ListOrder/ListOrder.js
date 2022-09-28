4b358bd9cb19ba788c43a716cc3a27242d9680f9
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-09-28 22:14:15 +0600

    complete add break section

--- src/components/ListOrder/ListOrder.js
+++ src/components/ListOrder/ListOrder.js
@@ -8,7 +8,16 @@ const ListOrder = () => {
             <p>Junior Web Developer</p>
             <p>Age: 22</p>
         </div>
-       
+       <div className="add-break">
+        <h4>Add a Break</h4>
+        <div>
+            <button><span>10</span>s</button>
+            <button><span>20</span>s</button>
+            <button><span>30</span>s</button>
+            <button><span>40</span>s</button>
+            <button><span>50</span>s</button>
+        </div>
+       </div>
         </div>
     );
 };
